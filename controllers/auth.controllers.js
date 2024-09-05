82d114626a4c9b26bd9037d64408f911e02b03f5
Devansh <devanshjindal31@gmail.com>
2024-09-05 20:59:15 +0530

    jwt removed

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,7 +1,6 @@
 import { validationResult } from "express-validator";
 import User from "../models/user.js";
 import bcrypt from "bcryptjs"
-import generateToken from "../utils/generateToken.js";
 
 export const register = async(req, res) => {
     const errors = validationResult(req);
@@ -27,15 +26,12 @@ export const register = async(req, res) => {
             password:hashedPassword
         })
 
-        const token = generateToken(newUser._id)
-    
         await newUser.save()
     
         res.status(201).json({
             _id: newUser._id,
             name: newUser.name,
             email: newUser.email,
-            token
         })
     } catch (error) {
         console.log("Error in register controller", error.message);
@@ -57,13 +53,10 @@ export const login = async (req, res) => {
             return res.status(401).json({ error: "Incorrect Username or Password"})
         }
 
-        const token = generateToken(user._id)
-    
         res.status(200).json({
             _id: user._id,
             name: user.name,
             email: user.email,
-            token
         })
     } catch (error) {
         console.log("Error in login controller:", error.message);
--- db/connectToDataBase.js
+++ db/connectToDataBase.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectToDataBase = async() => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/careerApp")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("connected to db");
     } catch (error) {
         console.log(error.message);
